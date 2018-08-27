740dd9759b2dac5459c928abf3621551b9f41d6e
Aubrey Hewes <aubrey@kingsquare.nl>
2018-08-27 11:13:21 +0200

    use eslint and a specific in-project configuration + added editorconfig

--- test/test-suite.js
+++ test/test-suite.js
@@ -4,28 +4,28 @@ var fs = require('fs');
 var path = require('path');
 
 var TEST_DIR = path.resolve(__dirname, 'fixtures');
-var SKIP_TEST = ['change resolution scope'];
+var SKIP_TEST = [ 'change resolution scope' ];
 
 var files = fs.readdirSync(TEST_DIR).filter(function (x) { return x.indexOf('.json') !== -1; });
 var tests = [];
 
 env.addSchema('http://json-schema.org/draft-04/schema', require('./draft-04-schema.json'));
-env.addSchema('http://localhost:1234/integer.json', {type: 'integer'});
+env.addSchema('http://localhost:1234/integer.json', { type: 'integer' });
 env.addSchema('http://localhost:1234/subSchemas.json', {
   'integer': { 'type': 'integer' },
   'refToInteger': { '$ref': '#/integer' }
 });
 
 function runTest (i, j, k) {
-  var schema = tests[i][j].schema;
-  var test = tests[i][j].tests[k];
+  var schema = tests[ i ][ j ].schema;
+  var test = tests[ i ][ j ].tests[ k ];
   it(test.description, function () {
     if (test.valid) { expect(env.validate(schema, test.data)).to.be.equal(null); } else { expect(env.validate(schema, test.data)).not.to.be.equal(null); }
   });
 }
 
 function runSuite (i, j) {
-  var suite = tests[i][j];
+  var suite = tests[ i ][ j ];
   if (SKIP_TEST.indexOf(suite.description) !== -1) { return; }
   describe(suite.description, function () {
     for (var k = 0, len = suite.tests.length; k < len; k++) { runTest(i, j, k); }
@@ -34,11 +34,11 @@ function runSuite (i, j) {
 
 function runGroup (i) {
   return function () {
-    for (var j = 0, len = tests[i].length; j < len; j++) { runSuite(i, j); }
+    for (var j = 0, len = tests[ i ].length; j < len; j++) { runSuite(i, j); }
   };
 }
 
 for (var i = 0; i < files.length; i++) {
-  tests.push(require(TEST_DIR + '/' + files[i]));
-  describe(files[i], runGroup(i));
+  tests.push(require(TEST_DIR + '/' + files[ i ]));
+  describe(files[ i ], runGroup(i));
 }
